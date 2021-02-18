6e466fddf5c96e525fdd7da804471e1b2ba7ccb0
alazhar <me@alazhar.xyz>
2021-02-19 04:43:23 +0700

    update data

--- src/config.js
+++ src/config.js
@@ -7,41 +7,38 @@ const profile = {
   about: {
     title: `HEY THERE! 
     I'M AZHAR`,
-    description: `Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-    veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-   
-    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis.`,
+    description: ``,
     findMeOn: [
       {
         iconName: 'location',
-        text: 'Location, Place',
+        text: 'Jakarta, Indonesia',
         url: '',
         hoverColor: '#546e7a'
       },
       {
         iconName: 'twitter',
-        text: '@username',
-        url: 'https://twitter.com/username',
+        text: '@alazhaar_',
+        url: 'https://twitter.com/alazhaar_',
         hoverColor: '#1da1f2'
       },
       {
         iconName: 'email',
-        text: 'email@email.com',
-        url: 'mailto://email@email.com',
+        text: 'me@alazhar.dev',
+        url: 'mailto://me@alazhar.dev',
         hoverColor: '#546e7a'
-      },
-      {
-        iconName: 'lock',
-        text: '@kuser',
-        url: 'https://keybase.io/kuser',
-        hoverColor: '#e9641c'
       }
+      // {
+      //   iconName: 'lock',
+      //   text: '@kuser',
+      //   url: 'https://keybase.io/kuser',
+      //   hoverColor: '#e9641c'
+      // }
     ]
   },
   social: [
     {
       iconName: 'twitter',
-      url: 'https://twitter.com/username',
+      url: 'https://twitter.com/alazhaar_',
       text: "I'm on Twitter",
       hoverColor: '#1da1f2'
     },
@@ -68,31 +65,31 @@ const profile = {
 
 const projects = [
   {
-    title: 'Project Title #1',
+    title: 'Test',
     img: '',
     description: 'Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.',
-    categories: ['Sample', 'Open source', 'React'],
-    siteUrl: 'https://amazing.project.com',
-    sourceCodeUrl: 'https://github.com/user/project'
-  },
-  {
-    title: 'Project Title #2',
-    img: 'https://www.nethruster.com/assets/img/icons/apple-touch-icon.png',
-    description: 'Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.',
-    categories: ['Sample', 'Freeware', 'Go'],
-    siteUrl: 'https://amazing.project.com'
-  },
-  {
-    title: 'Project Title #3',
-    img: 'https://www.nethruster.com/assets/img/wareader.svg',
-    description: 'Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.',
-    categories: ['Wat', 'Linux', 'LoL'],
-    sourceCodeUrl: 'https://github.com/user/:O'
+    categories: ['Sample'],
+    siteUrl: 'https://project.alazhar.dev/test/',
+    sourceCodeUrl: 'https://github.com/alazhar/test'
   }
+  // {
+  //   title: 'Project Title #2',
+  //   img: 'https://www.nethruster.com/assets/img/icons/apple-touch-icon.png',
+  //   description: 'Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.',
+  //   categories: ['Sample', 'Freeware', 'Go'],
+  //   siteUrl: 'https://amazing.project.com'
+  // },
+  // {
+  //   title: 'Project Title #3',
+  //   img: 'https://www.nethruster.com/assets/img/wareader.svg',
+  //   description: 'Lorem ipsum dolor sit amet, in nulla hendrerit ius, sed et congue graeci, qui id propriae accusata.',
+  //   categories: ['Wat', 'Linux', 'LoL'],
+  //   sourceCodeUrl: 'https://github.com/user/:O'
+  // }
 ]
 
 const formUrl = 'http://localhost:4000'
-const ReCAPTCHAKey = '6LcBOC8UAAAAAM9YRyBp1RR-1NnwvMU8UDsR63Vu'
+const ReCAPTCHAKey = '6LflCl4aAAAAAFaNzjMxUI5QcABpdF_ORZdLRIUW'
 // The basepath is the assets folder
 /// the file sould be imported in assets-import.js
 const missingProjectIcon = 'no-work-icon.jpg'
